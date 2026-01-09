47caeaa938f272ad796ad435528d1dfc0e7e0c3a
ravan1010 <ravanravana177@gmail.com>
2026-01-09 00:54:43 -0800

    main

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,34 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import Details from './routes/details.js'
 
+// import { fileURLToPath } from "url";
+
+
 const app = express();
 
 // Connect to Database - You must ADD parentheses () to call the function
-app.use(cors())
-app.use(express.json());
+app.use(express.urlencoded({extended:true, limit: '200mb'}))
+app.use(express.json({limit: '200mb' }));
+
+app.use(cors({
+  origin: true, // reflects request origin automatically
+  credentials: true
+}));
 
 // Use the imported routes
 app.use('/api', Details);
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+
+
+// app.use(express.static(path.join(__dirname, "../frontend/dist")))
+// app.get("/slug", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// })
+
+
 
 const PORT = 3000;
 mongoose.connect('mongodb+srv://suhasnayaj_db_user:YGrnAW1flbWdv1g0@iot-aipay.ded0rc6.mongodb.net/?appName=IOT-AIpay')
